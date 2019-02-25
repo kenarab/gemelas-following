9483bcea58cd49ed6471be6fa8e119a094508d0e
alejandro baranek <alejandrobaranek@gmail.com>
2019-02-25 10:12:04 -0300

    Last dataviz of weight gain

--- R/drafts/process_weights.R
+++ R/drafts/process_weights.R
@@ -1,5 +1,6 @@
 library(ggplot2)
 library(reshape2)
+library(dplyr)
 
 source.filename <- paste("~/gemelas_neonatologia_pesos.csv")
 weights.data <- read.csv(file = source.filename)
@@ -15,7 +16,12 @@ weights.data$prev.date <- c(as.Date(NA), weights.data$date[-nrow(weights.data)])
 weights.data$prev.date
 weights.data$diff.date <- difftime(weights.data$date, weights.data$prev.date)
 
+#projection
+i <- nrow(weights.data)+1
+weights.data[i,"date"] <- as.Date("2019-02-26")
+weights.data[i,"diff.date"] <- weights.data[i,"date"]-weights.data[i-1,"date"]
 
+weights.data.norm %>% filter(date=="2019-02-22")
 
 
 weights.data.norm <- weights.data[,c("date", "Sofia", "diff.date")]
@@ -38,6 +44,9 @@ weights.data.norm$daily.rate <- round(weights.data.norm$rel.weight^
                                            4)
 weights.data.norm$log.weight <- log(weights.data.norm$weight, base =2)
 
+
+
+
 dcast(weights.data.norm,
       formula = name~date,
       value.var = "daily.rate",
@@ -45,6 +54,7 @@ dcast(weights.data.norm,
 
 ggplot <- ggplot(data = weights.data.norm) +
   geom_line(aes(x = date, y = daily.rate, group = name, color = name))+
+  geom_smooth(aes(x = date, y = daily.rate, group = name, color = name))+
   ggtitle("Sofia y Margarita- Daily rate")
 ggplot
 ggsave(paste("~/gemelas_daily_rate.png"), ggplot)
@@ -52,21 +62,20 @@ ggsave(paste("~/gemelas_daily_rate.png"), ggplot)
 
 ggplot <- ggplot(data = weights.data.norm) +
   geom_line(aes(x = date, y = log.weight, group = name, color = name))+
-  ggtitle("Sofia y Margarita- Log weight")
+  geom_smooth(aes(x = date, y = log.weight, group = name, color = name))+
+  ggtitle("Sofia y Margarita- Log weight base 2")
 #g
 ggplot
 ggsave(paste("~/gemelas_log_weight.png"), ggplot)
 
-
-
-
-
-ggplot <- ggplot(data = weights.data) +
-          ggtitle("Sofia y Margarita- weights") +
-            geom_line(aes(x = date, y = Sofia, color="Sofia")) + #, label = "Sofia"
-            geom_line(aes(x = date, y = Margarita, color="Margarita")) #, label = "Margarita"
+ggplot <- ggplot(data = weights.data.norm) +
+  geom_line(aes(x = date, y = weight, group = name, color = name))+
+  geom_smooth(aes(x = date, y = weight, group = name, color = name))+
+  ggtitle("Sofia y Margarita- Weight")
+#g
 ggplot
+ggsave(paste("~/gemelas_weight.png"), ggplot)
 
-ggsave(paste("~/gemelas_pesos.png"), ggplot)
 
-plot(weights.data$Fecha, weights.data$Sofia, type = "l")
+
+
